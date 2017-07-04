0bf77a94070070b2355afca321a4f6978c1178a1
Rockman1233 <rocki67@me.com>
2017-07-04 18:09:48 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /home/box/web/etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /home/box/web/etc/init.d/nginx restart
