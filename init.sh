9458ae2e0476947deb11fb2a5614b2cb6e5e9c6c
Rockman1233 <rocki67@me.com>
2017-07-09 01:01:05 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/hello.py /usr/local/lib/python2.7/hello.py
 sudo gunicorn -b 0.0.0.0:8080 hello
