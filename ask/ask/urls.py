0bd0f4996a1f7cec562e384c03c262f24c20c110
Rockman1233 <rocki67@me.com>
2017-07-10 22:58:34 +0300

    urls from lesson

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,4 +18,9 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$login/', include('qa.urls')),
+    url(r'^$question/id1', include('qa.urls')),
+    url(r'^$ask/', include('qa.urls')),
+    url(r'^$popular/', include('qa.urls')),
+    url(r'^$new/', include('qa.urls')),
 ]
