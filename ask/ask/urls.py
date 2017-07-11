fc9a04ef4d05edbb4edb0bcbdec4c6d3f9607004
Rockman1233 <rocki67@me.com>
2017-07-11 23:31:12 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$login/', include('qa.urls')),
-    url(r'^$question/id1', include('qa.urls')),
-    url(r'^$ask/', include('qa.urls')),
-    url(r'^$popular/', include('qa.urls')),
-    url(r'^$new/', include('qa.urls')),
+    url(r'^$login/', include('login.urls')),
+    url(r'^$question/id1', include('question/id1.urls')),
+    url(r'^$ask/', include('ask.urls')),
+    url(r'^$popular/', include('popular.urls')),
+    url(r'^$new/', include('new.urls')),
 ]
