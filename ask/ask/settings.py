b618c06a4fd046fc82e601d1a750ff61a4e0a2e1
Rockman1233 <rocki67@me.com>
2017-07-12 22:49:31 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9!iplm$*3#%s980@i!cy9fy=c-n7wc$*gy6)piwbk53nvy*vrk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
