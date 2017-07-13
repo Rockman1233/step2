cd6d5430632d27042bf473fbef87284e2d69ff38
Rockman1233 <rocki67@me.com>
2017-07-13 23:34:59 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,5 @@ sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/gunicorn.conf.py
 gunicorn -c /etc/gunicorn.d/gunicorn.conf.py ask.wsgi:application
+
+curl -v http://127.0.0.1/
