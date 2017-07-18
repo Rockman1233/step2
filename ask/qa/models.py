554ba3a7e09a67b343558a5ab31048e10409f01b
Rockman1233 <rocki67@me.com>
2017-07-18 23:14:54 +0300

    add 2 models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+  title = models.CharField()
+  text = models.TextField()
+  added_at = models.DateField()
+  rating = models.FloatField()
+  author = models.CharField()
+  likes = models.TextField()
+
+class Answer(models.Model):
+  text = models.TextField()
+  added_at = models.DateField()
+  author = models.CharField()
