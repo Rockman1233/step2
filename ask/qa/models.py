9b671adc1eaac92b26a34bb9a914b713049a4d4a
Rockman1233 <rocki67@me.com>
2017-07-20 20:42:00 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Question(models.Model):
+    objects = QuestionManager()
     title = models.CharField(max_length = 30)
     text = models.TextField()
     added_at = models.DateField(auto_now_add = True)
