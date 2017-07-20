8033f7240b47f15f3180e7146501abbe7682ec3a
Rockman1233 <rocki67@me.com>
2017-07-20 20:43:41 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class QuestionManager(models.Manager):
+    def new(self):
+        pass
+    def popular(self):
+        pass
+
 class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length = 30)
@@ -10,14 +16,7 @@ class Question(models.Model):
     rating = models.IntegerField(default = 0)
     author = models.ForeignKey(User, null = True, on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User, related_name="q_to_likes")
-    
-class QuestionManager(models.Manager):
-    def new(self):
-        pass
-    def popular(self):
-        pass
-        
-        
+            
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateField()
