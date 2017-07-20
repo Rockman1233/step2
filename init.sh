7176a3e3d93c6bdfa2326e18a7d8e0a82a04ac96
Rockman1233 <rocki67@me.com>
2017-07-20 19:39:01 +0300

    some addings

--- init.sh
+++ init.sh
@@ -4,4 +4,7 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/gunicorn.conf.py
 gunicorn -c /etc/gunicorn.d/gunicorn.conf.py ask.wsgi:application
 
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database first_stepik_db"
+
 curl -v http://127.0.0.1/
