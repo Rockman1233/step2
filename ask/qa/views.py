eb83b0af8ff342ee75f1f5ce30e164bc90477e00
Rockman1233 <rocki67@me.com>
2017-07-24 22:27:26 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+def mainpg(request):
+	qs = Question.objects.order_by('-id')
+	page, paginator = paginate(request, qs)
+	paginator.baseurl = reverse('mainpg')
+	return render(request, 'main.html', {
+		'questions': page.object_list,
+		'page': page,
+		'paginator': paginator,
+	})
