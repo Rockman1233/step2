feba897b9b50cfd7c114b2da27d855040aa11a4e
Rockman1233 <rocki67@me.com>
2017-07-24 21:47:42 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,5 +5,3 @@ sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/gunicorn.conf.py
 gunicorn -c /etc/gunicorn.d/gunicorn.conf.py ask.wsgi:application
 
 sudo /etc/init.d/mysql start
-
-curl -v http://127.0.0.1/
