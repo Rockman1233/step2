8c964e3f0556c266052a78ba9835ea831db073b6
Rockman1233 <rocki67@me.com>
2017-07-25 22:33:20 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -11,6 +11,24 @@ from qa.models import Question
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
+def paginate(request, qs):
+	try:
+		limit = int(request.GET.get('limit', 10))
+	except ValueError:
+		limit = 10
+	if limit > 100:
+		limit = 10
+	try:
+		page = int(request.GET.get('page', 1))
+	except ValueError:
+		raise Http404
+	paginator = Paginator(qs, limit)
+	try:
+		page = paginator.page(page)
+	except EmptyPage:
+		page = paginator.page(paginator.num_pages)
+	return page, paginator
+
 def mainpg(request):
 	qs = Question.objects.order_by('-id')
 	page, paginator = paginate(request, qs)
