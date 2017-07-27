fdaa15b9e8a22092e57aa47f57c2e16f8c02e7cd
Rockman1233 <rocki67@me.com>
2017-07-27 19:37:38 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from qa.views import test, mainpg, question, popular
+from qa.views import test, mainpg, question, popular, question_ans
 
 urlpatterns = [
 	url(r'^$', mainpg, name='mainpg'),
